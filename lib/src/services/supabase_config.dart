163db35b729bf43bebb28f4be9b4e4e2cda63ffc
Reyhan <hannn4you@gmail.com>
2026-04-08 21:28:30 +0700

    API

--- lib/src/services/supabase_config.dart
+++ lib/src/services/supabase_config.dart
@@ -4,8 +4,7 @@ class SupabaseConfig {
   // Supabase Configuration
   static const String supabaseUrl = 'https://vvvcwxialnbzrcaitkql.supabase.co';
   static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2dmN3eGlhbG5ienJjYWl0a3FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0NzgwNzgsImV4cCI6MjA5MTA1NDA3OH0.SHsEmozD6qNMEC2rq5kBhNvFTZFPwLp7dGuPyotpn10
-';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2dmN3eGlhbG5ienJjYWl0a3FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0NzgwNzgsImV4cCI6MjA5MTA1NDA3OH0.SHsEmozD6qNMEC2rq5kBhNvFTZFPwLp7dGuPyotpn10';
 
   static Future<void> initialize() async {
     await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
